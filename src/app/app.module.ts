1bb4bd8adf9d819eeae2ff51ddd10d93ca908f1a
Jriera <jordiriera81@gmail.com>
2021-12-20 15:47:43 +0100

    chore:import product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
