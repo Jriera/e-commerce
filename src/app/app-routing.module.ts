841b334d213637cd7d29c80e54d91dbe96292c6b
Jriera <jordiriera81@gmail.com>
2021-12-20 15:48:05 +0100

    feat:Add routing to product detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path:':category', component:ProductListComponent}
+  {path:':category', component:ProductListComponent},
+  {path:':category/:id', component:ProductDetailComponent}
 ];
 
 @NgModule({
