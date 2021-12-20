016297fb77cde06f257d7b8512cd4569b3a79042
Jriera <jordiriera81@gmail.com>
2021-12-20 01:49:48 +0100

    chore:Add the params service to receive the updated params

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,78 +1,62 @@
-import { Component,OnDestroy, OnInit } from '@angular/core';
-import { forkJoin,Subscription} from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { forkJoin, Subscription } from 'rxjs';
 import { Product } from 'src/app/models/product';
 import { HttpService } from 'src/app/services/http.service';
 import { ActivatedRoute } from '@angular/router';
-import { trigger,transition,animate,state,style } from '@angular/animations';
-
+import { UrlParamsService } from 'src/app/services/url-params.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
-  
-})//end of component decorator
-export class ProductListComponent implements OnInit,OnDestroy {
-
-  productList:Product[] = [];
+}) //end of component decorator
+export class ProductListComponent implements OnInit, OnDestroy {
+  productList: Product[] = [];
   prductsSubscription = new Subscription();
-  categorySelected:string='electronics';
-  expand: boolean=false;
+  categorySelected: string = 'electronics';
+  expand: boolean = false;
 
-  constructor( private http:HttpService, private activatedRoute:ActivatedRoute) { }
+  constructor(
+    private http: HttpService,
+    private activatedRoute: ActivatedRoute,
+    private paramService: UrlParamsService
+  ) {}
 
   ngOnInit(): void {
-    
     this.getCategory();
-    
   }
 
- 
   ngOnDestroy(): void {
     this.prductsSubscription.unsubscribe();
   }
 
-  //following are the methods for requests to the API
-  getCategory(){
-    this.activatedRoute.params.subscribe(params=>{ 
-      this.categorySelected = params['category'];
-     
-      this.productList = [];
-      this.getPorductsList(this.categorySelected);
+  
+  getCategory() {// gets params from url 
+    this.activatedRoute.params.subscribe((params) => {
+      this.categorySelected = params['category'];//sets the params to the categorySelected
+      this.paramService.set(this.categorySelected);//sets the params to the urlParamsService
+
+      this.productList = [];//clears the productList
+      this.getProductsList(this.categorySelected);
     });
   }
-      
-  getPorductsList(category:string):void{
-    
-    if(category==='clothing'){
+
+
+//following are the methods for requests to the API
+  getProductsList(category: string): void {
+    if (category === 'clothing') {
       const menClothing = this.http.getMenClothingProducts().pipe();
       const womenClothing = this.http.getWomenClothingProducts().pipe();
-      const result = forkJoin(menClothing,womenClothing);
-      result.subscribe(data=>{
+      const result = forkJoin(menClothing, womenClothing);
+      result.subscribe((data) => {
         this.productList = data[0].concat(data[1]);
       });
-    }else{
-      this.prductsSubscription = this.http.getProductsByCategory(category).subscribe(data=>{
-        this.productList = data;
-      });
+    } else {
+      this.prductsSubscription = this.http
+        .getProductsByCategory(category)
+        .subscribe((data) => {
+          this.productList = data;
+        });
     }
   }
-
-
 }
-
-
-     
-    
-    
-    
-    
-
-    
-   
-    
-     
-
-  
-     
-
