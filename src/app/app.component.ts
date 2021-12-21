0594b81acb8d3c27c4d2ee74807f9178a72c9841
Jriera <jordiriera81@gmail.com>
2021-12-21 16:21:14 +0100

    feat:Add home url  detection method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,39 @@
 import { animate, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   animations: [
-    trigger('routeAnim',[
-      transition('* => *',[
-        style({
-          opacity:0,
-        }),//end of style
-        animate('1.3s ease-in')
-    ]),//end of transition
-  ]//end of trigger options
-)//end of trigger
-]//end of animations
+    trigger(
+      'routeAnim',
+      [
+        transition('* => *', [
+          style({
+            opacity: 0,
+          }), //end of style
+          animate('1.3s ease-in'),
+        ]), //end of transition
+      ] //end of trigger options
+    ), //end of trigger
+  ], //end of animations
 })
 export class AppComponent {
   title = 'e-commerce';
 
+  constructor(private router: Router) {}
+
   prepareRoute(outlet: RouterOutlet) {
-    if(outlet.isActivated){
-      return outlet.activatedRoute.snapshot.url
+    if (outlet.isActivated) {
+      return outlet.activatedRoute.snapshot.url;
     }
     return null;
-    
+  }
+
+  isHomeRoute() {
+    return this.router.url === '/';
   }
 }
