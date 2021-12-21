72b98d3186b1d893c1b5f33657c9582542df3270
Jriera <jordiriera81@gmail.com>
2021-12-21 16:19:04 +0100

    feat: Add basic nav menu

---
+++ src/app/components/basic-nav/basic-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-nav',
+  templateUrl: './basic-nav.component.html',
+  styleUrls: ['./basic-nav.component.scss']
+})
+export class BasicNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
