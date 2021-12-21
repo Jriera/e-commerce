e35df3a64f5fc7fd683ac57e295fb3af4192bdef
Jriera <jordiriera81@gmail.com>
2021-12-21 18:07:54 +0100

    feat:create fade in animation with gsap

---
+++ src/app/directives/gsap/fade-in.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, ElementRef, OnInit } from '@angular/core';
+import { CoreAnimationDirective } from './core-animation.directive';
+
+@Directive({
+  selector: '[fadeInAnimation]'
+})
+export class FadeInDirective extends CoreAnimationDirective implements OnInit {
+
+  constructor(protected override  element:ElementRef) { 
+    super(element);
+  }
+
+  ngOnInit() {
+    this.animateIn();
+  }
+
+  protected override animateIn() {
+    this.timeline.from(this.element.nativeElement,this.duration,{opacity:'0', ease:"Expo.easeInOut"}, this.delay);
+    super.animateIn();
+  }
+
+}
