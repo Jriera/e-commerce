719c3d772553d78f7aa45ab329f5123676ff574f
Jriera <jordiriera81@gmail.com>
2021-12-24 03:55:10 +0100

    feat:Add scroll animations to home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,180 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewChecked, Component, OnDestroy, OnInit } from '@angular/core';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy, AfterViewChecked {
+  st: globalThis.ScrollTrigger | null = null;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    gsap.registerPlugin(ScrollTrigger);
+
+    this.scrollAnims();
+    this.st?.enable();
+    console.log('init');
   }
 
+  ngAfterViewChecked() {
+    console.log('after view checked');
+    ScrollTrigger.refresh(); // since we're using ScrollTrigger, we need to refresh it due to the way angular handles routing without page refresh
+  }
+
+  scrollAnims() {
+    gsap.fromTo(
+      //animation for ring highlight
+      '.highlight-narrow',
+      {
+        scrollTrigger: '.highlight-narrow',
+        x: '-1000',
+        opacity: 0,
+        duration: 1.5,
+      },
+      {
+        scrollTrigger: '.highlight-narrow',
+        x: '0',
+        opacity: 1,
+        duration: 1.5,
+      }
+    );
+
+    gsap.fromTo(
+      //animation for hard drive highlight
+      '.HD',
+      {
+        scrollTrigger: {
+          trigger: '.HD',
+        },
+        y: '200',
+        opacity: 0.7,
+        duration: 1.5,
+      },
+      {
+        scrollTrigger: {
+          trigger: '.HD',
+        },
+        y: '0',
+        opacity: 1,
+        duration: 1.5,
+      }
+    );
+
+    gsap.fromTo(
+      //animation for backpack highlight
+      '.image-pill',
+      {
+        scrollTrigger: {
+          trigger: '.image-pill',
+        },
+        rotateY: '-90deg',
+        duration: 1.2,
+      },
+      {
+        scrollTrigger: {
+          trigger: '.image-pill',
+        },
+        rotateY: '0deg',
+        duration: 1.2,
+      }
+    );
+
+    gsap.fromTo(
+      // animation for department sections
+      '.container',
+      {
+        scrollTrigger: {
+          trigger: 'app-department',
+        },
+        rotateY: '-90deg',
+        duration: 1,
+        stagger: 0.2,
+      },
+      {
+        scrollTrigger: {
+          trigger: 'app-department',
+        },
+        rotateY: '0deg',
+        duration: 1,
+        stagger: 0.2,
+      }
+    );
+
+    gsap.fromTo(
+      // animation for department sections
+      '.shopping-image',
+      {
+        scrollTrigger: {
+          trigger: '.info-trigger',
+        },
+        rotateY: '-90deg',
+        duration: 2,
+        stagger: 0.2,
+      },
+      {
+        scrollTrigger: {
+          trigger: '.info-trigger',
+        },
+        rotateY: '0deg',
+        duration: 2,
+        stagger: 0.2,
+      }
+    );
+
+    gsap.fromTo(
+      // animation for department sections
+      '.info-title',
+      {
+        scrollTrigger: {
+          trigger: '.info-trigger',
+        },
+        x: '-500',
+        opacity: 0,
+        duration: 2,
+        stagger: 0.2,
+      },
+      {
+        scrollTrigger: {
+          trigger: '.info-trigger',
+        },
+        x: '0',
+        duration: 2,
+        opacity: 1,
+        stagger: 0.2,
+      }
+    );
+
+    gsap.fromTo(
+      // animation for department sections
+      '.info-description',
+      {
+        scrollTrigger: {
+          trigger: '.info-trigger',
+        },
+        
+        opacity: 0,
+        duration: 2,
+        delay:2
+        
+      },
+      {
+        scrollTrigger: {
+          trigger: '.info-trigger',
+        },
+        
+        duration: 2,
+        opacity: 1,
+        delay:2
+        
+      }
+    );
+
+    
+  }
+
+  ngOnDestroy() {}
 }
