6c0a81ba18e4693b599ea8c84672cc6c6262986b
Jriera <jordiriera81@gmail.com>
2021-12-24 03:53:15 +0100

    Create store info component

---
+++ src/app/components/store-info/store-info.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { gsap } from 'gsap';
+
+@Component({
+  selector: 'app-store-info',
+  templateUrl: './store-info.component.html',
+  styleUrls: ['./store-info.component.scss']
+})
+export class StoreInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
