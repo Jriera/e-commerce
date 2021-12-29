4e22c49570e9e9a07771d59229c1798783cbf562
Jriera <jordiriera81@gmail.com>
2021-12-29 16:53:15 +0100

    chore:define path for checkout component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
+  {path:'checkout', component:CheckoutComponent},
   {path:':category', component:ProductListComponent},
-  {path:':category/:id', component:ProductDetailComponent}
+  {path:':category/:id', component:ProductDetailComponent},
+ 
 ];
 
 @NgModule({
