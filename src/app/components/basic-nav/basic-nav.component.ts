f5a72b770ba522289bb8de795de1dd467bd40eb2
Jriera <jordiriera81@gmail.com>
2021-12-29 21:21:31 +0100

    fix add the basic cart functionality to basic nav

--- src/app/components/basic-nav/basic-nav.component.ts
+++ src/app/components/basic-nav/basic-nav.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, Subscription } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
+import { UrlParamsService } from 'src/app/services/url-params.service';
+import { CartComponent } from '../cart/cart.component';
 
 @Component({
   selector: 'app-basic-nav',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicNavComponent implements OnInit {
 
-  constructor() { }
+  expand: boolean = false; //controls the header expansion
+  categorySelected:string|null ='' //controls the header title
+  items$:Observable<number>|null = null; //Observable to keep track of cart length
+  items:number = 0; //Variable modfied by the subscription to the cart length observable
+  count = new Subscription(); // subscription that updates the items variable and is destroyed on component destruction
+
+  constructor(
+    private paramService:UrlParamsService,
+    private cartService:CartService,
+    private mDialog:MatDialog
+  ) { }
+
+
+   
 
   ngOnInit(): void {
+    this.getCategory();
+    this.items$ = this.cartService.getItems();
+    this.count = this.cartService.getItems().subscribe((items)=>{
+      this.items = items;
+    })
+  }
+
+  openCart(){
+    this.mDialog.open(CartComponent,{ 
+      width: '400px',
+      height: '600px',
+      backdropClass: 'backdrop'
+  });
+  }
+    
+
+    
+
+  getCategory(){
+    this.paramService.get().subscribe((params)=>{
+      this.categorySelected = params
+    })
+  }
+
+ 
+ 
+
+
+
+  //following are the methods for animations
+  expander(){
+   this.expand=!this.expand;
+  }
+
+  ngOnDestroy(){
+    this.count.unsubscribe();
   }
 
 }
