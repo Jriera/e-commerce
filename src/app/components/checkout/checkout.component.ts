5b1eacd7ceaa517f95896c9616dd422c1d8cf8fd
Jriera <jordiriera81@gmail.com>
2021-12-30 17:27:45 +0100

    chore: remove initial values for form controls

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -21,20 +21,20 @@ export class CheckoutComponent implements OnInit {
   }
 
   checkoutForm = this.fb.group({
-    name:['name',Validators.required],
-    email:['email',Validators.required],
-    phone:['phone',Validators.required],
+    name:['',Validators.required],
+    email:['',Validators.required,Validators.email],
+    phone:['',Validators.required],
     
-    address:['address',Validators.required],
-    zipCode:['00000'],
-    city:['city',Validators.required],
-    country:['country',Validators.required],
+    address:['',Validators.required],
+    zipCode:[''],
+    city:['',Validators.required],
+    country:['',Validators.required],
    
     
-    cardNumber:['1234567891234567',Validators.required],
-    cardName:['card name',Validators.required],
-    cardExpiry:['expiry',Validators.required],
-    cardCvc:['123',Validators.required],
+    cardNumber:['',Validators.required],
+    cardName:['',Validators.required],
+    cardExpiry:['',Validators.required],
+    cardCvc:['',Validators.required],
     
   })
 
