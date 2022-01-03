fcf2b9eb2d60d7e8f0f8beb1ecf0bfe409ca2b6f
Jriera <jordiriera81@gmail.com>
2022-01-03 01:56:50 +0100

    feat: create minibar nav for user options display

--- src/app/components/basic-nav/basic-nav.component.ts
+++ src/app/components/basic-nav/basic-nav.component.ts
@@ -1,69 +1,87 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Observable, Subscription } from 'rxjs';
+import { User } from 'src/app/models/user';
 import { CartService } from 'src/app/services/cart.service';
+import { FirebaseService } from 'src/app/services/firebase.service';
 import { UrlParamsService } from 'src/app/services/url-params.service';
 import { CartComponent } from '../cart/cart.component';
 
 @Component({
   selector: 'app-basic-nav',
   templateUrl: './basic-nav.component.html',
-  styleUrls: ['./basic-nav.component.scss']
+  styleUrls: ['./basic-nav.component.scss'],
 })
 export class BasicNavComponent implements OnInit {
-
   expand: boolean = false; //controls the header expansion
-  categorySelected:string|null ='' //controls the header title
-  items$:Observable<number>|null = null; //Observable to keep track of cart length
-  items:number = 0; //Variable modfied by the subscription to the cart length observable
+  categorySelected: string | null = ''; //controls the header title
+  items$: Observable<number> | null = null; //Observable to keep track of cart length
+  items: number = 0; //Variable modfied by the subscription to the cart length observable
   count = new Subscription(); // subscription that updates the items variable and is destroyed on component destruction
+  user: User | null = null;
+  userId: string | null = null;
 
   constructor(
-    private paramService:UrlParamsService,
-    private cartService:CartService,
-    private mDialog:MatDialog
-  ) { }
-
-
-   
+    private paramService: UrlParamsService,
+    private cartService: CartService,
+    private mDialog: MatDialog,
+    private firebaseService: FirebaseService
+  ) {}
 
   ngOnInit(): void {
     this.getCategory();
     this.items$ = this.cartService.getItems();
-    this.count = this.cartService.getItems().subscribe((items)=>{
+    this.count = this.cartService.getItems().subscribe((items) => {
       this.items = items;
-    })
+    });
+    this.logStatus();
+    this.currentUser();
   }
 
-  openCart(){
-    this.mDialog.open(CartComponent,{ 
+  openCart() {
+    // opens the cart dialog
+    this.mDialog.open(CartComponent, {
       width: '400px',
       height: '600px',
-      backdropClass: 'backdrop'
-  });
-  }
-    
-
-    
-
-  getCategory(){
-    this.paramService.get().subscribe((params)=>{
-      this.categorySelected = params
-    })
+      backdropClass: 'backdrop',
+    });
   }
 
- 
- 
-
-
-
-  //following are the methods for animations
-  expander(){
-   this.expand=!this.expand;
+  getCategory() {
+    //gets the category from the url
+    this.paramService.get().subscribe((params) => {
+      this.categorySelected = params;
+    });
   }
 
-  ngOnDestroy(){
+  expander() {
+    //toggles the header expansion
+    this.expand = !this.expand;
+  }
+
+  ngOnDestroy() {
     this.count.unsubscribe();
   }
 
+  logStatus() {
+    //checks if the user is logged in
+    this.firebaseService.currentUser$.subscribe((userID) => {
+      console.log(userID);
+      this.userId = userID;
+      userID ? true : false;
+    });
+  }
+
+  currentUser() {
+    //gets the current logged in user
+    this.firebaseService.user$.subscribe((user) => {
+      this.user = user;
+    });
+  }
+
+  signOut() {
+    //signs out the user
+    this.firebaseService.signOut();
+    /* this.user = null; */
+  }
 }
