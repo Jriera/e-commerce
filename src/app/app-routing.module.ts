58ba9cef7780ca08df4dcb0ad368b5694add88bf
Jriera <jordiriera81@gmail.com>
2022-01-13 16:33:43 +0100

    chore: create route for profile component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { CheckoutComponent } from './components/checkout/checkout.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path:'checkout', component:CheckoutComponent},
   {path:'auth', component:AuthComponent},
+  {path:'profile', component:ProfileComponent},
   {path:':category', component:ProductListComponent},
   {path:':category/:id', component:ProductDetailComponent},
   
@@ -17,7 +19,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled'})],
+  imports: [RouterModule.forRoot(routes,{scrollPositionRestoration: 'enabled'})],//this is to reset to top of window on every route change
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
