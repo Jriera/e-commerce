58c58e59f8dbddaecb7debbe4950a90d4d535e4b
Jriera <jordiriera81@gmail.com>
2022-01-19 16:46:39 +0100

    feat: Add order component responsible for displaying the orders of each user

---
+++ src/app/components/orders/orders.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Order } from 'src/app/models/order';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.scss']
+})
+export class OrdersComponent implements OnInit {
+
+  @Input() userOrders:Order[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
