743400bb84ad7412f16913739f575e01a8812396
Jriera <jordiriera81@gmail.com>
2022-01-19 16:37:11 +0100

    feat: Add a service to query the orders database

---
+++ src/app/services/order-http.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Order } from '../models/order';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderHttpService {
+
+  constructor(private http:HttpClient) { }
+
+  getOrders(){
+    return this.http.get('https://nest-commerce-api.herokuapp.com/orders');
+  }
+
+  getOrdersByUserId(userId:string):Observable<Order[]>{
+    return this.http.get<Order[]>(`https://nest-commerce-api.herokuapp.com/orders/by-user?userId=${userId}`);
+  }
+
+  saveOrder(order:Order){
+    
+    return this.http.post('https://nest-commerce-api.herokuapp.com/orders',{
+      "cart":order.cart,
+      "totalPrice":order.totalPrice,
+      "orderDate":order.orderDate.toDateString(),
+      "userId":order.userId,
+      "orderStatus":order.orderStatus
+
+  });
+}
+
+}
