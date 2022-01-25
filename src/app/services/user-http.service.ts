23890825e811491e42ba9f371b1238f432616f2d
Jriera <jordiriera81@gmail.com>
2022-01-25 18:46:46 +0100

    feat: Create method and observables to track admin status of users

--- src/app/services/user-http.service.ts
+++ src/app/services/user-http.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserHttpService {
+  admin:boolean  = false;
+  adminSubj = new BehaviorSubject(this.admin);
+  admin$ = this.adminSubj.asObservable();
 
   constructor(private http:HttpClient) { }
 
@@ -18,6 +22,27 @@ export class UserHttpService {
       "admin":false
     });
   }
+
+  getUsers():Observable<User[]>{
+    return this.http.get<User[]>('https://nest-commerce-api.herokuapp.com/users');
+  }
+
+  isAdmin(uid:string){
+    
+    this.getUsers().subscribe(users=>{
+      const selectedUser=users.filter(user=>user.uid ===uid)
+      if(selectedUser[0].admin){
+        this.admin=true;
+        this.adminSubj.next(this.admin);
+      }
+      
+    });
+
+
+  }
+
+      
+    
 
   
 }
